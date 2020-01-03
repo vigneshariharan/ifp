bea53c23135be297ab8b3fb3da87431446e2a02a
Vignesh Hariharan <47920969+vigneshariharan@users.noreply.github.com>
2020-01-03 22:20:56 +0530

    Update googleimages.py

--- googleimages.py
+++ googleimages.py
@@ -1,48 +1,53 @@
-# importing google_images_download module
-from google_images_download import google_images_download
+def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_between_interactions:int=1):
+    def scroll_to_end(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_interactions)    
+    
+    # build the google query
+    search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
 
-# creating object
-response = google_images_download.googleimagesdownload()
+    # load the page
+    wd.get(search_url.format(q=query))
 
-search_queries =["deer"]
+    image_urls = set()
+    image_count = 0
+    results_start = 0
+    while image_count < max_links_to_fetch:
+        scroll_to_end(wd)
 
+        # get all image thumbnail results
+        thumbnail_results = wd.find_elements_by_css_selector("img.rg_ic")
+        number_results = len(thumbnail_results)
+        
+        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+        
+        for img in thumbnail_results[results_start:number_results]:
+            # try to click every thumbnail such that we can get the real image behind it
+            try:
+                img.click()
+                time.sleep(sleep_between_interactions)
+            except Exception:
+                continue
 
-def downloadimages(query):
-    # keywords is the search query
-    # format is the image file format
-    # limit is the number of images to be downloaded
-    # print urs is to print the image file url
-    # size is the image size which can
-    # be specified manually ("large, medium, icon")
-    # aspect ratio denotes the height width ratio
-    # of images to download. ("tall, square, wide, panoramic")
-    arguments = {"keywords": query,
-                 "format": "jpg",
-                 "limit": 10,
-                 "print_urls": True,
-                 "size": "medium",
-                 "aspect_ratio": "panoramic"}
-    try:
-        response.download(arguments)
+            # extract image urls    
+            actual_images = wd.find_elements_by_css_selector('img.irc_mi')
+            for actual_image in actual_images:
+                if actual_image.get_attribute('src'):
+                    image_urls.add(actual_image.get_attribute('src'))
 
-    # Handling File NotFound Error
-    except FileNotFoundError:
-        arguments = {"keywords": query,
-                     "format": "jpg",
-                     "limit": 10,
-                     "print_urls": True,
-                     "size": "medium"}
+            image_count = len(image_urls)
 
-        # Providing arguments for the searched query
-        try:
-            # Downloading the photos based
-            # on the given arguments
-            response.download(arguments)
-        except:
-            pass
+            if len(image_urls) >= max_links_to_fetch:
+                print(f"Found: {len(image_urls)} image links, done!")
+                break
+        else:
+            print("Found:", len(image_urls), "image links, looking for more ...")
+            time.sleep(1)
+            load_more_button = wd.find_element_by_css_selector(".ksb")
+            if load_more_button:
+                wd.execute_script("document.querySelector('.ksb').click();")
 
+        # move the result startpoint further down
+        results_start = len(thumbnail_results)
 
-# Driver Code
-for query in search_queries:
-    downloadimages(query)
-    print()
+    return image_urls
